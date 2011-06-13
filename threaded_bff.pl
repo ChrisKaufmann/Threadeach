66104cf2c46e116fe7df717130465aab63d89f11
Chris Kaufmann <ckaufmann@connamara.com>
2011-06-13 09:36:49 -0500

    Added STOP functionality

--- threaded_bff.pl
+++ threaded_bff.pl
@@ -2,26 +2,29 @@
 use warnings;
 use strict;
 use Threadeach;
+use forks;
 
 my $start=shift || usage();
 my $end=shift || usage();
 my $threads=shift || 10;
 
-print "Running with start=$start, end=$end, $threads threads\n";
 my @lines=Threadeach::threadsome(\&run_bff,$threads,$start..$end);
 
 sub run_bff
 {
 	my $id=shift;
+	if(-e "STOP")	#end if the stop file exists (and remove it for laziness)
+	{
+		print "Found 'STOP' file, stopping main thread, others will finish\n";
+		unlink("STOP");
+		last;
+	}
 	my $cookie=$id%$threads;
 	my $cmd="bash bff.sh $id cookie$cookie.txt";
 	print "$cmd\n";
 	system($cmd);
-	$id=undef;
-	$cookie=undef;
-	$cmd=undef;
+	unlink("cookie$cookie.txt");
 }
-`rm cookie*.txt`;
 
 sub usage
 {
