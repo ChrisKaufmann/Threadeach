57956584a0da9cc6c5d3e968faa42e6b4ab0c8cd
Chris Kaufmann <ckaufmann@connamara.com>
2011-06-13 09:31:59 -0500

    Changed Threadeach to use forks instead of threads, and threaded_bff to not need chunks anymore

--- Threadeach.pm
+++ Threadeach.pm
@@ -1,7 +1,8 @@
 package Threadeach;
+use forks;
 use warnings;
 use strict;
-use threads;
+#use threads;
 use vars qw($VERSION);
 $VERSION='0.001';
 
@@ -33,30 +34,24 @@ sub threadsome
 	my $sub_handle=shift;
 	my $how_many=shift;
 	my @todos=@_;
-	my @returns = (); #it'll return everything in the same order as passed.  :)
-
-	my %list=();	#for maintaining the order as passed
 	my $todo_index=0;
 
 	#as long as there's anything running, waiting, or more to do
 	while(threads->list(threads::running) || threads->list(threads::joinable) || $todo_index < @todos)
 	{
-		#first, join all the ones that are waiting to be joined back in
+		#join all the ones that are waiting to be joined back in
 		foreach my $thr(threads->list(threads::joinable))
 		{
-			#and put any returned data into the returns array in the right spot.
-			@returns[$list{$thr->tid()}]=$thr->join();
+			$thr->join();
 		}
 		#as long as there's more to do (and we're not over the limit), spawn more
 		while($todo_index < @todos && threads->list(threads::running) < $how_many)
 		{
 			my $thr=threads->create($sub_handle,$todos[$todo_index]);
-			$list{$thr->tid()}=$todo_index;
 			$todo_index++;
 		}
 		sleep(1); #this is just so it doesn't spin its' wheels too fast while waiting
 	}
-	return @returns;
 }
 
 1;
--- threaded_bff.pl
+++ threaded_bff.pl
@@ -6,23 +6,9 @@ use Threadeach;
 my $start=shift || usage();
 my $end=shift || usage();
 my $threads=shift || 10;
-my $max_groupsize=shift || $end-$start;
 
 print "Running with start=$start, end=$end, $threads threads\n";
-if($end-$start > $max_groupsize)
-{
-	my $tmp_start=$start;
-	while($tmp_start < $end)
-	{
-		my $tmp_end=$tmp_start+$max_groupsize > $end?$end:$tmp_start+$max_groupsize;
-		system("./$0 $tmp_start $tmp_end $threads $max_groupsize");
-		$tmp_start+=$max_groupsize;
-	}
-}
-else
-{
-	my @lines=Threadeach::threadsome(\&run_bff,$threads,$start..$end);
-}
+my @lines=Threadeach::threadsome(\&run_bff,$threads,$start..$end);
 
 sub run_bff
 {
@@ -39,6 +25,6 @@ sub run_bff
 
 sub usage
 {
-	print "usage: $0 <startid> <endid> [Optional: # of threads] [optional: max groupsize]\n";
+	print "usage: $0 <startid> <endid> [Optional: # of threads]\n";
 	exit();
 }
